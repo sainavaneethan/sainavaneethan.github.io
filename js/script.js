5ebb67ab8f6e6bce1fa9ba5aee64f2a0548145aa
unknown <sainavaneethan@gmail.com>
2022-04-03 18:55:27 +0530

    bot questions added

--- js/script.js
+++ js/script.js
@@ -8,6 +8,43 @@
  /* ==============================================
   	 Main-Slider
   	=============================================== */ 
+
+            function google(stateWrapper, ready) {
+            window.open("https://google.com");
+            ready();
+        }
+        function bing(stateWrapper, ready) {
+            window.open("https://bing.com");
+            ready();
+        }
+        var rollbackTo = false;
+        var originalState = false;
+        function storeState(stateWrapper, ready) {
+            rollbackTo = stateWrapper.current;
+            console.log("storeState called: ",rollbackTo);
+            ready();
+        }
+        function rollback(stateWrapper, ready) {
+            console.log("rollback called: ", rollbackTo, originalState);
+            console.log("answers at the time of user input: ", stateWrapper.answers);
+            if(rollbackTo!=false) {
+                if(originalState==false) {
+                    originalState = stateWrapper.current.next;
+                        console.log('stored original state');
+                }
+                stateWrapper.current.next = rollbackTo;
+                console.log('changed current.next to rollbackTo');
+            }
+            ready();
+        }
+        function restore(stateWrapper, ready) {
+            if(originalState != false) {
+                stateWrapper.current.next = originalState;
+                console.log('changed current.next to originalState');
+            }
+            ready();
+        }
+
 $(document).ready(function() {
 $.validator.setDefaults({
 		submitHandler: function() {
@@ -41,39 +78,8 @@ $.validator.setDefaults({
         });
  
 
-            var count = 0;
-            var convForm = $('#chat').convform({eventList:{onInputSubmit: function(convState, ready) {
-                console.log('input is being submitted...');
-                //here you send the response to your API, get the results and build the next question
-                //when ready, call 'ready' callback (passed as the second parameter)
-                if(convState.current.answer.value==='end') {
-                    convState.current.next = false;
-                    //emulating random response time (100-600ms)
-                    setTimeout(ready, Math.random()*500+100);
-                } else {
-                    if(Array.isArray(convState.current.answer)) var answer = convState.current.answer.join(', ');
-                    else var answer = convState.current.answer.text;
-                    convState.current.next = convState.newState({
-                        type: 'select',
-                        noAnswer: true,
-                        name: 'dynamic-question-'+count,
-                        questions: ['This question state was built on your previous answer (you answered: '+answer+') and doesnt expect an answer'],
-                    });
-                    convState.current.next.next = convState.newState({
-                        type: 'select',
-                        name: 'dynamic-question-'+count,
-                        questions: ['This question state was built on your previous answer (you answered: '+answer+')'],
-                        answers: [
-                            {text: 'Answer 1', value: '1'},
-                            {text: 'Answer 2', value: '2'},
-                            {text: 'END', value: 'end'}
-                        ]
-                    });
-                    //emulating random response time (100-600ms)
-                    setTimeout(ready, Math.random()*500+100);
-                }
-                count++;
-            }}});
+           convForm = $('#chat').convform({selectInputStyle: 'disable'});
+            console.log(convForm);
 
 
 
