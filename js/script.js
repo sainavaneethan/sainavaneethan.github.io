d23e7d8ec39005c3e6e992ceaff6b76cef9dba82
unknown <sainavaneethan@gmail.com>
2022-04-03 18:37:28 +0530

    bot added

--- js/script.js
+++ js/script.js
@@ -14,6 +14,69 @@ $.validator.setDefaults({
 			// alert("submitted!");
 		}
 	});
+ $(".select2_el").select2({
+            });
+        //Toggle fullscreen
+        $(".chat-bot-icon").click(function () {
+            $(this).children('img').toggleClass('hide');
+            $(this).children('svg').toggleClass('animate');
+            $('.chat-screen').toggleClass('show-chat');
+            $('.chat-bodyf').toggleClass('hide');
+             $('.chat-header-option').toggleClass('hide');
+        });
+        $('.chat-mail button').click(function () {
+            $('.chat-mail').addClass('hide');
+            $('.chat-body').removeClass('hide');
+            $('.chat-bodyf').removeClass('hide');
+            $('.chat-input').removeClass('hide');
+            $('.chat-header-option').removeClass('hide');
+        });
+        $('.end-chat').click(function () {
+            $('.chat-body').addClass('hide');
+            $('.chat-bodyf').addClass('hide');
+            $('.chat-input').addClass('hide');
+            $('.chat-session-end').removeClass('hide');
+            $('.chat-header-option').addClass('hide');
+            $('.chat-screen').toggleClass('show-chat');
+        });
+ 
+
+            var count = 0;
+            var convForm = $('#chat').convform({eventList:{onInputSubmit: function(convState, ready) {
+                console.log('input is being submitted...');
+                //here you send the response to your API, get the results and build the next question
+                //when ready, call 'ready' callback (passed as the second parameter)
+                if(convState.current.answer.value==='end') {
+                    convState.current.next = false;
+                    //emulating random response time (100-600ms)
+                    setTimeout(ready, Math.random()*500+100);
+                } else {
+                    if(Array.isArray(convState.current.answer)) var answer = convState.current.answer.join(', ');
+                    else var answer = convState.current.answer.text;
+                    convState.current.next = convState.newState({
+                        type: 'select',
+                        noAnswer: true,
+                        name: 'dynamic-question-'+count,
+                        questions: ['This question state was built on your previous answer (you answered: '+answer+') and doesnt expect an answer'],
+                    });
+                    convState.current.next.next = convState.newState({
+                        type: 'select',
+                        name: 'dynamic-question-'+count,
+                        questions: ['This question state was built on your previous answer (you answered: '+answer+')'],
+                        answers: [
+                            {text: 'Answer 1', value: '1'},
+                            {text: 'Answer 2', value: '2'},
+                            {text: 'END', value: 'end'}
+                        ]
+                    });
+                    //emulating random response time (100-600ms)
+                    setTimeout(ready, Math.random()*500+100);
+                }
+                count++;
+            }}});
+
+
+
 $("#main-contact-form").validate({
     rules: {
       
